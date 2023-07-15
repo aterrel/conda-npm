77a5570577dbb82869f797e8e1d304c52f6e8fc5
Andy R. Terrel <andy.terrel@gmail.com>
2023-07-15 11:52:39 -0500

    Add install command

--- conda_npm/plugin.py
+++ conda_npm/plugin.py
@@ -1,10 +1,27 @@
 import argparse
 
 import conda.plugins
+from ruamel.yaml import YAML
+
+
+def add_package(package_spec: str)->None:
+    print(f"Adding package {package_spec}")
+    yaml = YAML(typ='safe')  # default, if not specfied, is 'rt' (round-trip)
+    env_doc = yaml.load(open('environment.yml').read())
+    env_doc['dependencies'].append(package_spec)
+    with open('environment.yml', 'w') as f:
+        yaml.dump(env_doc, f)
+    print(f"Added package {package_spec} to environment.yml")
+
+
+def handle_install(args: list[str]):
+    add_package(args[0])
 
 
 def conda_npm(args: list[str]):
-    print('I ran!')
+    if args[0] == "install":
+        print('handling install')
+        handle_install(args[1:])
 
 
 @conda.plugins.hookimpl
